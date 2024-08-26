d37b34376dee0ffc6f0824215ff498987eee6d0b
Himanshu Kumar <himanshukumar802123@gmail.com>
2024-08-26 12:38:28 +0530

    Input Box

--- src/App.js
+++ src/App.js
@@ -1,8 +1,19 @@
 import React, {Component} from "react";
+import CurrencyInput from "./components/CurrencyInput";
 
 class App extends Component {
+  state={
+    from: 'USD',
+    to: 'INR',
+    rate : 1,
+    fromAmt: 1,
+    toAmt: 1,
+  }
   render() {
-    return <div className="currency-converter"></div>;
+    return <div className="currency-converter">
+      <CurrencyInput/>
+      <CurrencyInput/>
+    </div>;
   }
 }
 
